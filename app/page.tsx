85aeeabdeb37c459a5f1061a79b2fefd6b43df57
Jagmans <jagmansidhu1@icloud.com>
2025-11-13 15:37:33 -0800

    Updated main page hero, TODO: need image

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,49 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import Link from "next/link";
+
+const MMHCTag = () => {
+    return (
+        <div className="flex flex-col md:flex-row items-center gap-8 md:gap-12 w-full max-w-6xl mx-auto px-4">
+
+            <div className="flex justify-center md:justify-start">
+                <Image
+                    src="/MMHC_cropped_logo.png"
+                    alt="UBC MMHC logo"
+                    width={400}
+                    height={400}
+                    className="rounded-full object-contain"
+                />
+            </div>
+
+            <div className=" flex flex-col gap-5 just">
+                <p className="text-5xl font-bold text-secondary text-shadow-2xs">UBC's</p>
+                <p className="text-5xl font-bold text-secondary text-shadow-2xs">Mens Mental Health Club</p>
+                <p className="text-xl font-medium text-secondary text-shadow-2xs">
+                    Community of peer support, open discussion, and self-improvement
+                </p>
+
+                <div className="flex flex-row justify-center gap-5 mt-4">
+                    <Link href="/memberregistration">
+                        <button className="px-6 py-3 bg-primary-bg text-secondary font-semibold rounded-lg shadow-md hover:bg-secondary-bg transition duration-300">
+                            Become a Member
+                        </button>
+                    </Link>
+                    <Link href="/events">
+                        <button className="px-6 py-3 bg-primary-bg text-secondary font-semibold rounded-lg shadow-md hover:bg-secondary-bg transition duration-300">
+                            Events
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const HomePage = () => {
-  const MMHCTag = () => {
-    return (
-      <div className="flex flex-col align-middle gap-15">
-        <p className="text-5xl font-bold text-white text-shadow-2xs">UBC's</p>
-        <p className="text-5xl font-bold text-white text-shadow-2xs">Mens Mental Health Club</p>
-        <p className="text-xl font-medium text-white text-shadow-2xs">
-          A community of dedicated peer support, open discussion, and self-improvement
-        </p>
-      </div>
-    );
-  };
-
   return (
     <div className="w-screen bg-primary-bg text-primary-text">
-      <Hero imageSrc="/Hero1.jpeg" children={MMHCTag()} />
+      <Hero imageSrc="/hero/Hero1.jpeg" children={MMHCTag()} />
       <div className="flex flex-col gap-10 mx-auto justify-center text-center max-w-[70vw] p-8">
         <div className="flex flex-col gap-5 items-baseline">
           <h1 className="text-4xl font-bold mb-4">About Us</h1>
