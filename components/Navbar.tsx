81fa8e4f6a82168824fbfa750b1e806655c05888
StashC <stashubc@student.ubc.ca>
2025-11-17 21:38:21 -0500

    remove opacity from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,59 +1,58 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import NavLink from "./NavLink";
 import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-    const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(true);
 
-    useEffect(() => {
-        let lastScrollY = window.scrollY;
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
 
-        const handleScroll = () => {
-            const currentScrollY = window.scrollY;
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-            if (currentScrollY < lastScrollY || currentScrollY < 10) {
-                setVisible(true);
-            } else {
-                setVisible(false);
-            }
-            lastScrollY = currentScrollY;
-        };
+      if (currentScrollY < lastScrollY || currentScrollY < 10) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+      lastScrollY = currentScrollY;
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return (
-        <nav
-            className={`
+  return (
+    <nav
+      className={`
                 fixed top-0 w-full z-50 shadow-lg 
-                bg-primary-bg/90 backdrop-blur-sm 
+                bg-primary-bg backdrop-blur-sm 
                 transition-all duration-400 ease-in-out
-                ${visible ? 'translate-y-0' : '-translate-y-full'}
-            `}
-        >
-            <div className="px-6 sm:px-8 lg:px-10 py-3 flex justify-between items-center">
-                <div className= "text-xl font-bold text-white tracking-wider">
-                    <Link href="/" className="flex items-center space-x-2">
-                        <Image src="/MMHC_cropped_logo.png" alt="UBC MMHC logo" width={30} height={30} className="rounded-full" />
-                        <span>UBCMMHC</span>
-                    </Link>
-                </div>
-                <div className="space-x-3">
-                    <NavLink href="/" text="Home"/>
-                    <NavLink href="/events" text="Events"/>
-                    <NavLink href="/login" text="LogIn"/>
-                    {/* <NavLink href="/about" text="About" /> */}
-                </div>
-            </div>
-        </nav>
-    );
+                ${visible ? "translate-y-0" : "-translate-y-full"}
+            `}>
+      <div className="px-6 sm:px-8 lg:px-10 py-3 flex justify-between items-center">
+        <div className="text-xl font-bold text-white tracking-wider">
+          <Link href="/" className="flex items-center space-x-2">
+            <Image src="/MMHC_cropped_logo.png" alt="UBC MMHC logo" width={30} height={30} className="rounded-full" />
+            <span>UBCMMHC</span>
+          </Link>
+        </div>
+        <div className="space-x-3">
+          <NavLink href="/" text="Home" />
+          <NavLink href="/events" text="Events" />
+          <NavLink href="/login" text="LogIn" />
+          {/* <NavLink href="/about" text="About" /> */}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
