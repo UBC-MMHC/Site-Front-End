8dc5712ec72b3689dbdca8b7ea7e8a345d1095c9
Richard <idk.duderc@gmail.com>
2025-11-27 19:35:53 -0800

    pulled backend api to env

--- app/constants.ts
+++ app/constants.ts
@@ -4,7 +4,8 @@ export const LINK_TREE = "https://linktr.ee/ubcmmhc";
 export const MEMBERSHIP_SIGNUP_URL = "https://forms.gle/Nq1NNWKjXafmFCrF7";
 export const EXEC_SIGNUP_URL = "https://forms.gle/Ys43WB2vfQc1UP6r7";
 
-export const BASE_URL_API = "http://localhost:8080";
+export const BASE_URL_API =
+  process.env.NEXT_PUBLIC_BASE_URL_API || "http://localhost:8080";
 export const API_ROUTES_URL = {
   login_email: `${BASE_URL_API}/api/auth/login-email`,
   login_google: `${BASE_URL_API}/api/auth/google`,
