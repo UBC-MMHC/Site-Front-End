e154184953b355fb10954df83d043fae9b62b785
Jagmans <jagmansidhu1@icloud.com>
2026-01-20 22:25:16 -0800

    Updated front page to redirect to membership page

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,7 +1,7 @@
 export const INSTAGRAM_URL = "https://www.instagram.com/ubcmmhc/";
 export const EMAIL = "ubcmmhc@gmail.com";
 export const LINK_TREE = "https://linktr.ee/ubcmmhc";
-export const MEMBERSHIP_SIGNUP_URL = "https://forms.gle/Nq1NNWKjXafmFCrF7";
+export const MEMBERSHIP_SIGNUP_URL = "/membership";
 export const EXEC_SIGNUP_URL = "https://forms.gle/Ys43WB2vfQc1UP6r7";
 export const LINKEDIN_URL = "https://www.linkedin.com/company/ubcmmhc/posts";
 
