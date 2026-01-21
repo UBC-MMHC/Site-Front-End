057919b0e80c084c5f01a68f75fd0246ca406038
Daggerpov <danielagapov1@gmail.com>
2026-01-21 15:08:57 -0800

    plugin for downgrading all eslint errors -> warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
+import "eslint-plugin-only-warn";
 
 const eslintConfig = defineConfig([
 	...nextVitals,
