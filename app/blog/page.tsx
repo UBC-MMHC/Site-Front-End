236c9f9639cecd487463e5b083d70faf3b30280f
StashC <stashubc@student.ubc.ca>
2026-02-23 12:40:20 -0800

    add github token to build

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,13 +7,15 @@ import { TEST_POSTS } from "./testdata";
 
 export default function BlogPage(): React.ReactElement {
 	return (
-		<div className="flex min-h-screen flex-col items-center px-4 pt-24">
-			<BlogMainPage
-				posts={TEST_POSTS}
-				getPostHref={function (post: BlogPostType): string {
-					return `/blog/${post.slug}`;
-				}}
-			/>
+		<div className="flex min-h-screen w-full flex-col items-center px-4 pt-24">
+			<div className="max-w-4xl">
+				<BlogMainPage
+					posts={TEST_POSTS}
+					getPostHref={function (post: BlogPostType): string {
+						return `/blog/${post.slug}`;
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
