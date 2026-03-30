3e348b213734c0ba0e3e2507bf307d780fba6db0
Stash Currie <89222842+StashC@users.noreply.github.com>
2026-03-29 18:23:18 -0700

    Add blogs (#27)

--- app/blog/blogData.ts
+++ app/blog/blogData.ts
@@ -37,10 +37,360 @@ Do you think you’d be consuming so much if your mind were more at-ease with yo
 When you are consuming, I’d urge you to be cognizant of how it can relate to your own creation, your own purpose. For example, by being intentional about which videos you watch, as they could directly relate to how you can apply it, whether in the present, or in the distant future doesn’t matter. What matters is that you can ease your mind with the feeling that this is indeed contributing to your purpose. What I’ve found to be helpful is bookmarking relevant content, and being aware of exactly how it can fit into my goals, whether career, personal learning of philosophy, and otherwise. Then, actually applying that into some ‘collage of learning’, some implementation of that learning, or by teaching it to others. You are then making good use of the content, instead of it using you (for engagement). 
 
 Another practical tip would be to disable, or avoid the temptation of succumbing to the algorithm. You can do this through disabling short-form content (like Instagram reels), disabling video recommendations (like on YouTube), and purely relying on your own search, as it brings about *thought and intentionality* behind your exploration, similar to how you would’ve thought about which book to look for in a library.`,
-		isFeatured: true,
+		isFeatured: false,
 		date: new Date("2026-02-26"),
 		image: "/hero/DiscussionTableHero.JPG",
 		tags: ["Blog", "Creativity"],
 		author: { name: "Daniel Agapov" },
 	},
+	{
+		slug: "life-audit",
+		title: "Life Audit: The Last Goal Setting System You Will Ever Need",
+		summary:
+			"Bold promise, but stick with me; this EXACT system has helped me get 30 million views on social media, a full-ride scholarship, transfer from SFU Criminology to UBC Sauder (13% acceptance rate), and run a half-marathon",
+		content: `Bold promise, but stick with me 
+
+This EXACT system has helped me get 30 million views on social media, a full-ride scholarship, transfer from SFU Criminology to UBC Sauder (*13% acceptance rate*), and run a half-marathon. 
+
+And last time I checked in the mirror, I’m not anyone special.
+
+If it works for me, then it can be for you too. 
+
+Only if you are open-minded and you’re willing to change. 
+
+This could work wonders on you.
+
+There’s a saying that goes *“you can lead a horse to water, but you can’t force it to drink.”* 
+
+After reading this whole document that I spent hours on 
+
+You can take action on it and improve your life 
+
+Or you can continue living life, chasing the next “self-help” book to make yourself feel “productive.” 
+
+Now that that’s out of the way, let's start
+
+**To begin, I want you to imagine a car…**
+
+There are **TWO PETALS: *Gas and Brake***  
+
+*Imagine driving as fast as you can but then your foot is stuck on the brake* 
+
+*Without realsing the brake, you’re stuck, and even if you’re going \- you’re going at 10km/hour* 
+
+*Have fun trying to get to your destination with that speed*
+
+## **Chapter 1: Releasing The Brake**
+
+Before you start with any goal setting, I want to think to yourself 
+
+And answer honestly 
+
+Only you will know if you’re lying to yourself or not 
+
+**“What is holding you back from where you want to be?”**
+
+I want you to pinpoint where you lack 
+
+*What limiting belief are you still holding onto?*
+
+*What bad habits are you currently doing that are holding you back?*
+
+Is it doom scrolling, porn, entertainment, Netflix, or lack of commitment
+
+You fill in the blank 
+
+Because of xyz, I am not where I want to be in life. 
+
+Take some time to think about this. 
+
+Because this might be the first time you’re reflecting in this area of life 
+
+And it takes practice to fully see through all the protective layers that your body has in place to find the root cause of your issues. 
+
+Don’t overthink too much and stay in the negative for too long
+
+This is only step 1 of the journey. 
+
+## **Chapter 2: The Gas Petal**  
+
+Before you can apply gas to your goals. 
+
+We first have to figure out where we’re trying to go 
+
+*Rome wasn’t built by accident; it was by design* 
+
+*Similarly, like the guy in the gym with the dream physique you want* 
+
+*That wasn’t an accident; it was built through meticulous training and careful design* 
+
+*If nothing happens by “accident.”* 
+
+***Then why are you living life like it was an accident?***
+
+Now I want you to reflect on **“What does success look like to you?”** 
+
+**Think big scale (ignore thinking “realistic”)** 
+
+**What do you want your future to look like**   
+I want you to design your life out as if it were perfected 
+
+The ideal timeframe I find worked best for me is 6-12 months (adjust it how you see fit)
+
+Since if you drag it out too long, you might tend to push it off, thinking it's too far out of reach (at least that’s how I thought)
+
+Now, I want you to close your eyes and visualize the type of lifestyle you would be living 
+
+Go into meticulous details, don’t leave anything out 
+
+The more detail you define now, the easier it is for you in the future to know *EXACTLY* where you are going
+
+*What type of friends would you be with* 
+
+*What do your finances look like* 
+
+*Where would you live* 
+
+*What does your physique/health look like* 
+
+*How does your social aspect (family/love life) look like* 
+
+*What car would you drive?* 
+
+*\*These are just guiding questions to ask yourself; they are not exhaustive\** 
+
+*You know your life better than I know your life.* 
+
+*The goal of these questions is to get you started, not finish the job* 
+
+Once you figure out where your end destination is, we can move on to the next step 
+
+This is similar to putting the *location* on Google Maps 
+
+## **Chapter 3 \- Previewing the Destination** 
+
+Sticking with the Google Map metaphor, once you know where you’re heading 
+
+Your dream reality \- of everything perfected 
+
+Now let’s work backwards 
+
+*If you knew all of your goals were achievable and that it would all be completely in the time frame you set yourself* 
+
+*How would your present self act now* 
+
+*What type of behaviour would you adopt?*
+
+*Would you still doomscroll for 4 hours?* 
+
+*Would you still act without a care in the world or tomorrow?*
+
+*Or would you act with a sense of purpose and urgency?* 
+
+*How would you feel?*
+
+*Would you still be anxious about whether you will hit your goal? (Because if you follow this system, it will be inevitable that you will hit your goals GUARANTEED)*
+
+*Or would you be calmed and relaxed knowing that your goals are achievable?*
+
+*Essentially, this is to construct a mental model from the BEST VERSION of yourself to the present self* 
+
+*You are quite LITERALLY transforming into that ideal/best version of yourself, so then you can operate from that standpoint instead of your present self, because if you do the same thing as you’re doing now* 
+
+*You will get the same results as now* 
+
+*“Insanity is repeating the same thing and expecting different results” \- Albert Einstein* 
+
+## **Chapter 4 \-  Creating the plan** 
+
+Imagine if you could create a plan that makes it IMPOSSIBLE for you NOT to succeed
+
+That sounds like a crazy promise, right 
+
+But hear me out… 
+
+Imagine your goal is to get 6-pack abs 
+
+But if we reverse engineer a system to make that goal impossible for you NOT to SUCCEED 
+
+It would go 
+
+- Train at the gym 3-4 times a week   
+- Progressively overload   
+- Eat healthy 
+
+On a long enough time frame 
+
+If you do that continuously, it will be physically impossible for you not to succeed. 
+
+Now for this part, I want you to forget the end goal 
+
+Forget the results 
+
+Now we will be focusing on the INPUT
+
+*What action can you take towards your goal that would make it impossible for you not to succeed* 
+
+*What type of input can you work towards the goal?*
+
+*The goal here is not to make it massive (run 5km per day if you haven’t run a day in your life and you want to run a half-marathon)* 
+
+*The point here is to build the consistency muscle* 
+
+*Stack your daily wins* 
+
+*So then, once you win enough days, you’ll win the week, and once you win enough weeks, you will win the year* 
+
+*We already established our MACRO viewpoint of where we want to be in life* 
+
+*Of who we want to become* 
+
+*But now this step is to drill down into the micro aspect* 
+
+*What can you do daily to make it impossible for you not to succeed* 
+
+*And you already know all the right things to do* 
+
+*The point is, how can you make sure to complete it every day* 
+
+***Extra tip: employ principles from Atomic Habits to make your habit stick: make it easy, make it obvious, make it satisfying, and make it attractive.*** 
+
+Build out a daily schedule that is **Reasonable** but doable for you to stick to 
+
+Time table or to-do list 
+
+Tracked by the hour or tracked by the day as a whole 
+
+This is for you to experiment and see what works best for you. 
+
+Experiment on yourself 
+
+Again, you know yourself best. 
+
+## **Chapter 5 \- Driving to the destination** 
+
+## **(*The Most Important Part*)** 
+
+Unfortunately, there is no substitute for this 
+
+You already have all the information
+
+You have the plan 
+
+Now the only missing piece of the puzzle   
+Is ACTUALLY driving the DAMN car to the location 
+
+Imagine saying to your friends that you will meet them at McDonald's 
+
+But you spend all the time worrying about what the most *OPTIMAL* route to save on gas is 
+
+How many right/left turns to take that optimizes xyz
+
+Imagine how ridiculous that sounds 
+
+The same way you do the same to your goals 
+
+You stand there…
+
+Waiting for the perfect moment…
+
+And it never hits.
+
+Another year goes by… 
+
+Same spot.
+
+Wishing for change. 
+
+But no matter how motivated I get you to feel from this document 
+
+You still have to do the work 
+
+Take the action 
+
+Every. Single. Day 
+
+And that will drive your success 
+
+You got this\!
+
+I believe in you :)
+
+**\-JP Zhou** 
+
+***follow @jpzhouoffical on all platforms if you find this useful***
+
+***Have a beautiful day***  `,
+		isFeatured: false,
+		date: new Date("2026-03-09"),
+		image: "/blogs/LifeAuditThumb.jpg",
+		tags: ["Discussion", "Goals", "Personal Growth"],
+		author: { name: "JP Zhou" },
+	},
+	{
+		slug: "cbt-introspection",
+		title: "Structured Introspection: Cognitive Behavioural Perspectives for Self-Insight",
+		summary:
+			"How our thoughts, emotions, and behaviours influence eachother and how you can use this for reflection.",
+		content: `## Context
+Cognitive Behavioural theories are largely attributed to a psychologist named Aaron Beck, who noticed that individuals who tended to be depressed also had cognitions and that tended to be negative, and indeed, ‘distorted’. 
+Eventually, Beck’s insights paved the way for early cognitive behavioural models, which in some sense structured our thoughts, emotions, and behaviours, and indeed, our very selves. 
+A lot of our discussions in the past have focused on discussing topics that may sometimes challenge our assumptions or give insight into how we think about something, but I thought it might be interesting to now directly focus on our patterns of thinking given any context of our choice. In other words, I thought it might be interesting to do some thinking about our thinking in a structured way, and my hope is to see if we can uncover some self-insight into our personal tendencies that may better inform our responses and actions in daily life. 
+
+### Prompt 1
+Cognitive Behavioural models are built on the premise that our thoughts, behaviours, and emotions all influence one another. Can you think of an example in your daily or personal life in which any one of these three factors significantly influences the other? 
+
+**Follow up:**
+
+Do you find that any of these 3 factors tend to usually precede the others? For example, might you often find that your behaviours or emotions follow your thoughts, or the other way around? Do you find this helpful, unhelpful, or anything in between?
+
+### Prompt 2
+We’re now going to dive a little deeper into the cognitive components of the model. Generally, there is thought to be three levels to cognition, we’ll first look at the first 2: 
+1. Conscious thoughts: Rational thoughts and choices that are made with full awareness.
+2. Automatic thoughts: Thoughts that flow rapidly, so that you may not be fully aware of them. This may mean you can’t check them for accuracy or relevance.
+
+Can you think of some examples in your daily life discerning these 2 different levels of cognition?
+
+**Follow up:**
+
+Do you ever find yourself trying to check your conscious thoughts for accuracy or relevance? Do you ever find yourself trying to check your automatic thoughts for accuracy or relevance?
+
+
+### Prompt 3
+The third level to cognition are our schemas: our core beliefs and personal rules for processing information, shaped by repeated influences. Think about some of the examples you thought of in the first and second questions. When you look at the content of your thoughts and the context, do you notice a pattern arising? In other words, do you notice how some of your thoughts surrounding certain situations might be based on one or more schemas?
+
+**Follow up:**
+Where might you think some of these schemas come from? Can you recall experiences which may have influenced them? Do you think the schemas that you have are accurate and/or helpful, or unhelpful?
+
+
+### Prompt 4
+Within Cognitive Behavioural models, there is a component of unhelpful thinking styles known as cognitive distortions. A few interesting (and sometimes relatable) ones for myself include:
+**All or nothing thinking: ** viewing situations as absolute (i.e. if I am not perfect I’m a failure)
+**Catastrophizing:** Exaggerating the importance of errors and to imagine the worst case scenario
+**Overgeneralization:** Taking a single negative event and seeing it as a never-ending pattern of defeat
+
+In consideration of the thoughts and schemas that we’ve discussed so far, can you think of any way in which they might be distorted in these specific ways?
+
+
+### Prompt 5
+The way we think is not always negatively distorted. Can you think of situations or events that make you feel empowered, positive, fulfilled, purposeful/meaningful, and are generally free from negative distortions or even ameliorates distortions? Why is this the case? 
+		`,
+		isFeatured: true,
+		date: new Date("2026-03-23"),
+		image: "/blogs/CBTDiscussionPic.png", // TODO
+		tags: ["Discussion", "Psychology"],
+		author: { name: "Jack Yeung" },
+	},
+	// TODO: need markdown
+	// {
+	// 	slug: "how-to-learn",
+	// 	title: "How to Learn",
+	// 	summary: "How to study in preparation for midterms and finals.",
+	// 	content: ``,
+	// 	isFeatured: false,
+	// 	date: new Date("2026-03-16"),
+	// 	image: "/blogs/HowToLearnGraphic2.png",
+	// 	tags: ["Discussion", "Learning", "School"],
+	// 	author: { name: "Alex Weitzel" },
+	// },
 ];
